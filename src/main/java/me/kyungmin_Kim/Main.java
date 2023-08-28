59aefd94f47db6672b6903a650e224d75e1c397a
doublekm153 <doublekm153@gmail.com>
2023-08-28 14:18:42 +0900

    test

--- src/main/java/me/kyungmin_Kim/Main.java
+++ src/main/java/me/kyungmin_Kim/Main.java
@@ -1,4 +1,4 @@
-package me.kyungmin-Kim;
+package me.kyungmin_Kim;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
--- src/main/java/me/kyungmin_Kim/Springboot_Study_backend/Springboot_Study_backend_Application.java
+++ src/main/java/me/kyungmin_Kim/Springboot_Study_backend/Springboot_Study_backend_Application.java
@@ -1,4 +1,7 @@
+package me.kyungmin_Kim.Springboot_Study_backend;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
 
 @SpringBootApplication
 public class Springboot_Study_backend_Application {
